8ddc990475fed0dd89ebe0f6834464b532e3fb0a
daniela <moreno.tobar.daniela@gmail.com>
2022-07-06 10:40:00 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import bg from '../public/assets/bg.png'
+
+function App() {
+
+
+  return (
+    <div className="App bg-bluedark max-w-screen-2xl mx-auto">
+      <div>
+        <img src={bg} />
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'bluedark': '#2C426B',
+        'bluelight': '#3AA1D6',
+        'green': '#8BD96E',
+        'yellow': '#FFECA1',
+        'dark': '#323232',
+      }
+    },
+  },
+  plugins: [],
+}
