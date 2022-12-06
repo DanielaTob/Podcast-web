6b8a8be79f42b1bd76de40ddbfd9d5dcc295f291
daniela <moreno.tobar.daniela@gmail.com>
2022-12-06 12:37:20 -0500

    new img

--- src/components/Insti.jsx
+++ src/components/Insti.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
-import imgkids from '/assets/imgp.jpg'
+import imgkids from '/assets/school.jpeg'
+import imgkids1 from '/assets/school1.jpeg'
+import imgkids3 from '/assets/schoolT.jpeg'
+import imgkids4 from '/assets/school4.jpeg'
+import imgkids5 from '/assets/school5.jpeg'
 
 const Insti = () => {
   return (
@@ -11,7 +15,7 @@ const Insti = () => {
                     <p className="font-normal text-base leading-6 text-dark bg-green px-4 py-4 rounded text-justify">La Institución Educativa Técnico Industrial Veinte de Julio, contribuye en la formación integral del estudiante en los niveles de educación preescolar, básica primaria y bachillerato técnico industrial; con pertinencia, calidad y valores para una armónica convivencia, con un enfoque de inclusión, mediante el desarrollo de competencias referidas al desempeño académico, laboral y socio-ambiental.</p>
                 </div>
                 <div className="w-full lg:w-8/12 ">
-                    <img className="w-full h-full" src="https://i.ibb.co/FhgPJt8/Rectangle-116.png" alt="A group of People" />
+                    <img className="w-full h-full" src={imgkids} alt="A group of People" />
                 </div>
             </div>
 
@@ -23,20 +27,20 @@ const Insti = () => {
                 <div className="w-full lg:w-8/12 lg:pt-8 ">
                     <div className="grid md:grid-cols-4 sm:grid-cols-2 grid-cols-1 lg:gap-4 shadow-lg rounded-md">
                         <div className="p-4 pb-6 flex justify-center flex-col items-center">
-                            <img className="md:block hidden" src={imgkids} alt="Alexa featured Img" />
-                            <img className="md:hidden block" src={imgkids} alt="Alexa featured Img" />
+                            <img className="md:block hidden" src={imgkids1} alt="Alexa featured Img" />
+                            <img className="md:hidden block" src={imgkids1} alt="Alexa featured Img" />
                         </div>
                         <div className="p-4 pb-6 flex justify-center flex-col items-center">
-                            <img className="md:block hidden" src={imgkids} alt="Olivia featured Img" />
-                            <img className="md:hidden block" src={imgkids} alt="Olivia featured Img" />
+                            <img className="md:block hidden" src={imgkids3} alt="Olivia featured Img" />
+                            <img className="md:hidden block" src={imgkids3} alt="Olivia featured Img" />
                         </div>
                         <div className="p-4 pb-6 flex justify-center flex-col items-center">
-                            <img className="md:block hidden" src={imgkids} alt="Liam featued Img" />
-                            <img className="md:hidden block" src={imgkids} alt="Liam featued Img" />
+                            <img className="md:block hidden" src={imgkids4} alt="Liam featued Img" />
+                            <img className="md:hidden block" src={imgkids4} alt="Liam featued Img" />
                         </div>
                         <div className="p-4 pb-6 flex justify-center flex-col items-center">
-                            <img className="md:block hidden" src={imgkids} alt="Elijah featured img" />
-                            <img className="md:hidden block" src={imgkids} alt="Elijah featured img" />
+                            <img className="md:block hidden" src={imgkids5} alt="Elijah featured img" />
+                            <img className="md:hidden block" src={imgkids5} alt="Elijah featured img" />
                         </div>
                     </div>
                 </div>
